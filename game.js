715ee4ae830dada2d0ee88133845c9c12e79ebff
yoonjookim <yjblossom@gmail.com>
2024-08-19 16:14:46 -0400

    Create new pattern in javaScript

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
-alert("Hello!"); //test
+const buttonColours = ["red", "blue", "green", "yellow"];
+
+const gamePattern = [];
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+
+    const randomChosenColour = buttonColours[randomNumber];
+    //Create a new variable called randomChosenColour and use the randomNumber from step 2 to select a random colour from the buttonColours array.
+
+    gamePattern.push(randomChosenColour);
+    //Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
+}
