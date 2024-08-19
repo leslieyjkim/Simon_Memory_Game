bde534154a3e2d58eb34cf53c6efdc5091e846e1
yoonjookim <yjblossom@gmail.com>
2024-08-19 16:30:22 -0400

    Show sequence to User with animation & sounds

--- game.js
+++ game.js
@@ -10,4 +10,15 @@ function nextSequence() {
 
     gamePattern.push(randomChosenColour);
     //Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    //The # in $("#" + randomChosenColour) is a selector in jQuery that refers to an HTML element's id attribute.
+    //"#" + randomChosenColour forms a string like "#red", "#blue", etc.
+    //So, $("#red") would select the element with id="red" in your HTML.
+    //visualize 0.1 sec visible -> 0.1 sec  invisible -> 0.1 sec visible
+
+    const audio = new Audio("./sounds" + randomChosenColour + ".mp3"); //For example, if randomChosenColour is "red", the code will create an audio object for the file "sounds/red.mp3".
+    audio.play(); //This method plays the audio file that was just created. When this line of code runs, the sound associated with the selected color will play.
+
+
 }
