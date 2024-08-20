458d2ba2bd2fba4ef12417431a056e7579a93a7b
yoonjookim <yjblossom@gmail.com>
2024-08-20 01:28:34 -0400

    Feature Start the Game with detecting keyboard has been pressed with updating value of level

--- game.js
+++ game.js
@@ -3,27 +3,41 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = []; 
 
+//you only call nextSequence() on the first keypress, and started.
+var started = false;
+
+var level = 0; //Once game started, the <h1>'s text would be updated to level.
+
+$(document).keypress(function() { //jQuery detect when any keyboard key has been pressed. 
+    if(!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
 
 $(".btn").click(function() {
-     //Inside the handler, you can use the keyword "this" to refer to the button object that triggered the click.
      var userChosenColour = $(this).attr("id");
      userClickedPattern.push(userChosenColour); 
-     //console.log(userClickedPattern);
+
      playSound(userChosenColour);
-     //when a user clicks on a button, the corresponding sound should be played.
      animatePress(userChosenColour);
 });
 
 function nextSequence() {
+
+    level++;  //inside nextSequence(), increase the level by 1 every time nextSequence() is called.
+    $("level-title").text("Level " + level); //inside nextSequence(), update the <h1> with this change in the value of level.
 
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
   
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
     playSound(randomChosenColour);
   }
+
 
 function playSound(name) {
     var audio = new Audio("sounds/" + name + ".mp3");
@@ -35,5 +49,5 @@ function animatePress(currentColour) {
 
     setTimeout(function() {
         $("#" + currentColour).removeClass("pressed");
-    }, 100); //after 0.1sec, remove the pressed class 
+    }, 100); 
 };
